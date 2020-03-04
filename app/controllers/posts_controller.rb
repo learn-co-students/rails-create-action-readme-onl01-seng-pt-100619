967505badcc6835a48d42646c4c09ca45ea45758
allisoncortez <allizetorc@gmail.com>
2020-03-04 16:39:45 -0600

    passed lab

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,9 +13,15 @@ class PostsController < ApplicationController
 
   # add create method here
   def create
-    post = Post.new
-    post.title = params[:title]
-    post.description = params[:description]
-    post.save
+    @post = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+
+    #each route will automatically check for a matching view..
+    # since we don't need a view for create, we must redirect
+    # after we are done saving the post... in order to avoid errors.
+    
+    redirect_to post_path(@post)
   end
 end
