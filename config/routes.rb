b6c53332d363939843c9bb12fb08d27631432a30
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-03-14 21:31:49 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #post "/posts", to: "posts#create"
 end
+
+# when the routes are added with resources and no exemptions there is already a create route for the model
